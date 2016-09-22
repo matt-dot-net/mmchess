f98e4646b3829696997e7f641bd819f68ae2c0bb
Matt McKnight <matt_mcknight@live.com>
2016-09-22 01:45:53 -0400

    SEE

--- src/mmchess/StaticExchange.cs
+++ src/mmchess/StaticExchange.cs
@@ -1,30 +1,32 @@
-namespace mmchess{
+// namespace mmchess{
 
-    // public static class StaticExchange{
-    //     public static int Eval(Board b, Move m)
-    //     {
-    //         ulong attackers, ulong[]xray_attackers;
+//     public static class StaticExchange{
+//         //evaluates exchanges on a square initiated by stm
+//         public static int Eval(Board b, Move m, int stm)
+//         {
+//             ulong attackers, ulong[]xray_attackers;
 
-    //         var sq = m.To;
-    //         var attacks = MoveGenerator.Attacks(b,sq);
-    //         var stm = b.SideToMove;
+//             var sq = m.To;
+//             var attacks = MoveGenerator.Attacks(b,sq); 
+//             var mystm = stm ^ 1; 
+//             attackers = attacks & b.Pieces[mystm];
 
-    //         attackers = attacks & b.Pieces[b.SideToMove];
-    //         //we've established which squares have the attackers
-    //         //to get xray attackers we remove these first attackers and compute again
-    //         while(attackers>0){
-    //             var attack_sq = attackers.BitScanForward();
-    //             attackers ^= BitMask.Mask[attack_sq];
+//             //we've established which squares have the attackers
+//             //to get xray attackers we remove these first attackers and compute again
+//             while(attackers>0){
+//                 var attack_sq = attackers.BitScanForward();
+//                 attackers ^= BitMask.Mask[attack_sq];
 
-    //             //find which piece it is and remove it for the next iteration
-    //             if((b.Pawns[stm] & attack_sq)>0)
+//                 //find which piece it is and remove it for the next iteration
+//                 //only rooks, bishops, queens can be xray sliders
+//                 if(b.Queens[mystm])
 
                 
-    //         }
+//             }
 
 
             
             
-    //     }
-    // }
-}
+//         }
+//     }
+// }
