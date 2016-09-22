86bd21c23241ab720b5c49f90ec37887a8d644eb
Matt McKnight <matt_mcknight@live.com>
2016-09-22 17:13:46 -0400

    Static Exchange Evaluation

--- src/mmchess/StaticExchange.cs
+++ src/mmchess/StaticExchange.cs
@@ -1,32 +1,100 @@
-// namespace mmchess{
+using System;
 
-//     public static class StaticExchange{
-//         //evaluates exchanges on a square initiated by stm
-//         public static int Eval(Board b, Move m, int stm)
-//         {
-//             ulong attackers, ulong[]xray_attackers;
+namespace mmchess
+{
 
-//             var sq = m.To;
-//             var attacks = MoveGenerator.Attacks(b,sq); 
-//             var mystm = stm ^ 1; 
-//             attackers = attacks & b.Pieces[mystm];
+    public static class StaticExchange
+    {
+        //evaluates exchanges on a square initiated by stm
+        public static int Eval(Board b, Move m, int stm)
+        {
 
-//             //we've established which squares have the attackers
-//             //to get xray attackers we remove these first attackers and compute again
-//             while(attackers>0){
-//                 var attack_sq = attackers.BitScanForward();
-//                 attackers ^= BitMask.Mask[attack_sq];
+            ulong[] attackers=new ulong[2];
+            int score=0;
+            var sq = m.To;
+            var pieceVal = Evaluator.PieceValueOnSquare(b,sq);
+            var attacks = MoveGenerator.Attacks(b, sq);
+            var mystm = stm ^ 1;
+            attackers[mystm] = attacks & b.Pieces[mystm];
+            attackers[stm] = attacks & b.Pieces[stm];
 
-//                 //find which piece it is and remove it for the next iteration
-//                 //only rooks, bishops, queens can be xray sliders
-//                 if(b.Queens[mystm])
 
+            //if there is a promotion, then stm is already up material
+            if(m.Promotion>0)
+                score= Evaluator.PieceValues[m.Promotion];
+
+            //add in any material taken on the first capture
+            if((m.Bits & (byte)MoveBits.Capture)>0)
+                score+=Evaluator.PieceValueOnSquare(b,m.To);
+
+            //change the pieceVal to be the new piece
+            pieceVal = Evaluator.PieceValues[(int)Move.GetPiece((MoveBits)m.Bits)];
+
+            //remove the first Move
+            attackers[stm] ^= BitMask.Mask[m.From];
+
+            //make a pass over the attackers to find the lowest valued piece
+            while (attackers[mystm] > 0)
+            {
+                var tempAttackers = attackers[mystm];
+                var leastValuable = int.MaxValue;
+                int leastValSq = -1;
+                while (tempAttackers > 0)
+                {
+                    var attack_sq = tempAttackers.BitScanForward();
+                    tempAttackers ^= BitMask.Mask[attack_sq];
+                    var attackerVal = Evaluator.PieceValueOnSquare(b, attack_sq);
+                    if (attackerVal < leastValuable)
+                    {
+                        leastValSq = attack_sq;
+                        leastValuable = attackerVal;
+                    }
+                }
+
+                //now we have the least valuable attacker
+                //remove whatever value is on the square
+                if(mystm==stm)
+                    score+=pieceVal;
+                else
+                    score-=pieceVal;
                 
-//             }
+                //update the attacked piece
+                pieceVal = leastValuable;
 
+                //remove the attacker
+                attackers[mystm] ^= BitMask.Mask[leastValSq];
 
-            
-            
-//         }
-//     }
-// }
+                //now we check for x-ray attacks 
+                //the idea here is that if any rooks, queens, or bishops attack the temporary
+                //square from the same direction that the temporary square is attacking the target
+                //then we have an xray attacker
+                var direction = MoveGenerator.Directions[leastValSq,sq];
+                ulong xrayAttacks=0;
+                switch(Math.Abs(direction)){
+                    case 1:
+                    case 8:
+                        if(((b.Rooks[mystm]|b.Queens[mystm]) & Board.FileMask[sq.File()])>0)
+                            xrayAttacks = MoveGenerator.RookAttacks(b,leastValSq) & (b.Rooks[mystm]|b.Queens[mystm]) & attackers[mystm];
+                        break;
+                    case 7:
+                    case 9:
+                            xrayAttacks = MoveGenerator.BishopAttacks(b,leastValSq) & (b.Bishops[mystm]|b.Queens[mystm]) & attackers[mystm];
+                        break;
+                }
+
+                while(xrayAttacks >0){
+                    var xraySq =xrayAttacks.BitScanForward();
+                    xrayAttacks ^= BitMask.Mask[xraySq];
+                    if(MoveGenerator.Directions[xraySq,sq]==direction){
+                        //put this piece in main attackers bitboard
+                        attackers[mystm] |= BitMask.Mask[xraySq];
+                    }
+                }
+                //switch stm
+                mystm ^= 1;
+            }
+
+            return score;
+        }
+    }
+}
