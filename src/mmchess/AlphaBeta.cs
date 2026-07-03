08bbca3a8e291a37f842db11514e843b1af595b1
Matt McKnight <mmcknight@torchtech.com>
2026-07-03 10:00:55 -0400

    Partial classing AlphaBeta to move Quiesce code

--- src/mmchess/AlphaBeta.cs
+++ src/mmchess/AlphaBeta.cs
@@ -5,7 +5,7 @@ using static mmchess.TranspositionTableEntry;
 
 namespace mmchess
 {
-    public class AlphaBeta
+    public partial class AlphaBeta
     {
         public const int MAX_DEPTH = 64;
         int Ply { get; set; }
@@ -37,11 +37,6 @@ namespace mmchess
             TimeLimit = TimeSpan.FromSeconds(5);
             Metrics = new AlphaBetaMetrics();
             Interrupt = interrupt;
-        }
-
-        int OrderQuiesceMove(Move m)
-        {
-            return LvaMvv(m);
         }
 
         int OrderRootMove(Move m)
@@ -107,66 +102,6 @@ namespace mmchess
                 Evaluator.PieceValues[(int)Move.GetPiece((MoveBits)m.Bits)])
                 +
                 Evaluator.PieceValues[m.Promotion]; //add promotion in as well
-        }
-
-        public int Quiesce(int alpha, int beta)
-        {
-            Metrics.Nodes++;
-            Metrics.QNodes++;
-
-            if (Ply >= MAX_DEPTH)
-            {
-                TakeBack();
-                return Evaluator.Evaluate(MyBoard,-10000,10000);
-            }
-
-            if ((Metrics.Nodes & 65535) == 65535)
-            {
-                Interrupt();
-                if (MyGameState.TimeUp)
-                    return alpha;
-            }
-            int score = Evaluator.Evaluate(MyBoard,alpha,beta);
-            if(score > alpha)
-                alpha = score;
-            //attemp to stand pat (don't search if eval tells us we are in a good position)
-            var inCheck = MyBoard.InCheck(MyBoard.SideToMove);
-            if (!inCheck)
-            {
-                if (score >= beta)
-                    return beta;
-
-                //Don't bother searching if we are evaluating at less than a Queen
-                if (score < alpha - Evaluator.PieceValues[(int)Piece.Queen])
-                    return alpha;
-
-            }
-
-            var moves = MoveGenerator
-                .GenerateCapturesAndPromotions(MyBoard,false)
-                .OrderByDescending((m) => OrderQuiesceMove(m));
-            
-            foreach (var m in moves)
-            {
-                if((BitMask.Mask[m.To] & (MyBoard.King[0]|MyBoard.King[1])) > 0)
-                    return beta;
-
-                //if SEE says this is a losing capture, we prune it
-                if(StaticExchange.Eval(MyBoard,m,MyBoard.SideToMove) < 0)
-                    continue;
-                MyBoard.MakeMove(m,false);
-                Ply++;
-
-                score = -Quiesce(-beta, -alpha);
-                TakeBack();
-                if (score >= beta)
-                    return beta;
-                if (score > alpha)
-                    alpha = score;
-
-            }
-            return alpha;
-
         }
 
         private Boolean Make(Move m)
--- src/mmchess/Quiesce.cs
+++ src/mmchess/Quiesce.cs
@@ -1,3 +1,72 @@
-namespace mmchess{
-    
+using System.Linq;
+
+namespace mmchess
+{
+    public partial class AlphaBeta
+    {
+        int OrderQuiesceMove(Move m)
+        {
+            return LvaMvv(m);
+        }
+
+        public int Quiesce(int alpha, int beta)
+        {
+            Metrics.Nodes++;
+            Metrics.QNodes++;
+
+            if (Ply >= MAX_DEPTH)
+            {
+                TakeBack();
+                return Evaluator.Evaluate(MyBoard,-10000,10000);
+            }
+
+            if ((Metrics.Nodes & 65535) == 65535)
+            {
+                Interrupt();
+                if (MyGameState.TimeUp)
+                    return alpha;
+            }
+            int score = Evaluator.Evaluate(MyBoard,alpha,beta);
+            if(score > alpha)
+                alpha = score;
+            //attemp to stand pat (don't search if eval tells us we are in a good position)
+            var inCheck = MyBoard.InCheck(MyBoard.SideToMove);
+            if (!inCheck)
+            {
+                if (score >= beta)
+                    return beta;
+
+                //Don't bother searching if we are evaluating at less than a Queen
+                if (score < alpha - Evaluator.PieceValues[(int)Piece.Queen])
+                    return alpha;
+
+            }
+
+            var moves = MoveGenerator
+                .GenerateCapturesAndPromotions(MyBoard,false)
+                .OrderByDescending((m) => OrderQuiesceMove(m));
+
+            foreach (var m in moves)
+            {
+                if((BitMask.Mask[m.To] & (MyBoard.King[0]|MyBoard.King[1])) > 0)
+                    return beta;
+
+                //if SEE says this is a losing capture, we prune it
+                if(StaticExchange.Eval(MyBoard,m,MyBoard.SideToMove) < 0)
+                    continue;
+                MyBoard.MakeMove(m,false);
+                Ply++;
+
+                score = -Quiesce(-beta, -alpha);
+                TakeBack();
+                if (score >= beta)
+                    return beta;
+                if (score > alpha)
+                    alpha = score;
+
+            }
+            return alpha;
+
+        }
+    }
 }
